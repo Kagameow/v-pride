0efb3137e1ef18ea682ea440da5ca72805c36b08
Kagameow <kagami.404@gmail.com>
2020-02-17 21:32:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="app">
+        <h1 v-pride.gradient="2000">v-pride Directive demo page</h1>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <div>
+            <h2>Gradient text mode:</h2>
+            <h3 v-pride.gradient>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam, aliquid
+                animi asperiores
+                assumenda atque, dolore dolores esse, exercitationem illo nostrum quibusdam ratione recusandae
+                repudiandae
+                rerum sit vel velit? Accusamus alias consectetur consequatur deleniti eius, error fugiat ipsam, magni
+                nesciunt nisi non nostrum numquam odit quas sint. Provident, sit temporibus. Ab consectetur dolore
+                dolores
+                et excepturi exercitationem facilis nostrum porro quos suscipit! Aspernatur, culpa cumque delectus
+                dolorem
+                excepturi exercitationem fugit illo libero, nobis numquam placeat provident quasi quos ratione
+                reprehenderit
+                rerum voluptatibus. Aspernatur cupiditate deleniti, dignissimos doloribus earum eligendi fugit harum
+                ipsum
+                libero magnam perferendis ratione sed soluta vero voluptatem.</h3>
+        </div>
+        <div>
+            <h2>Animated gradient text mode:</h2>
+            <h3 v-pride.animatedGradient>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam, aliquid
+                animi asperiores
+                assumenda atque, dolore dolores esse, exercitationem illo nostrum quibusdam ratione recusandae
+                repudiandae
+                rerum sit vel velit? Accusamus alias consectetur consequatur deleniti eius, error fugiat ipsam, magni
+                nesciunt nisi non nostrum numquam odit quas sint. Provident, sit temporibus. Ab consectetur dolore
+                dolores
+                et excepturi exercitationem facilis nostrum porro quos suscipit! Aspernatur, culpa cumque delectus
+                dolorem
+                excepturi exercitationem fugit illo libero, nobis numquam placeat provident quasi quos ratione
+                reprehenderit
+                rerum voluptatibus. Aspernatur cupiditate deleniti, dignissimos doloribus earum eligendi fugit harum
+                ipsum
+                libero magnam perferendis ratione sed soluta vero voluptatem.</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false;
+
+Vue.directive('pride', {
+    bind(el, binding) {
+        const applyRainbowStyling = (el) => {
+            el.style.webkitBackgroundClip = "text";
+            el.style.color = "transparent";
+            el.style.backgroundImage = "linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red)";
+        };
+        if (binding.modifiers['gradient'] || ['animatedGradient']) {
+            applyRainbowStyling(el);
+            if (binding.value) {
+                el.style.backgroundPosition = binding.value + "px";
+            }
+            if (binding.modifiers['animatedGradient']) {
+                el.style.backgroundSize = "800% 800%";
+                el.style.backgroundImage = "repeating-linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red, violet)";
+                let rainbowAnimation = [
+                    {backgroundPosition: '0% 50%'},
+                    {backgroundPosition: '100% 25%', offset: 0.5},
+                    {backgroundPosition: '0% 50%'}
+                ];
+                let rainbowTiming = {
+                    duration: 8000,
+                    iterations: Infinity,
+                    easing: "ease"
+                };
+                el.animate(
+                    rainbowAnimation,
+                    rainbowTiming
+                );
+                console.log(rainbowAnimation, rainbowTiming)
+            }
+        }
+    }
+});
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
